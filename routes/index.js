3936daff03f558f4ab662d15b466532f0916419b
W1NTER <83407276+ShabbyRug775@users.noreply.github.com>
2023-04-09 22:44:22 -0600

    Revert "Merge pull request #5 from Jajr16/LogIn"
    This reverts commit 61fa7c0f188f4494c73cfbce7b30c6f921b9e702, reversing
    changes made to 378c59b2a9f36b090f149f6dbe46ac643a320cd6.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. (index.ejs)*/
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Inicio - CLARAC' });
+  res.render('LogIn', { title: 'Inicie Sesión' });
 });
+
+/* GET home page. (nosotros.ejs)*/
+router.get('/Inicio', function(req, res, next) {
+  res.render('Inicio', { title: '--- Menú ---' });
+});
+
+/* GET home page. (Productos.ejs)*/
+router.get('/Productos', function(req, res, next) {
+  res.render('Productos', { title: 'Agregar productos' });
+});
+
 
 module.exports = router;
