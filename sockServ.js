17b13c8a4e7c92602be36e9fdd3144a51fc2b7b1
Jajr16 <jajr0316@gmail.com>
2023-04-17 20:19:31 -0600

    Update sockServ.js

--- sockServ.js
+++ sockServ.js
@@ -20,7 +20,7 @@ io.on('connection', (socket) => {
 
         console.log('Datos recibidos:', data);
         // Autenticar al usuario utilizando la lógica definida anteriormente
-        db.query(`select*from usuario where User = "${data.User}" and Pass = "${data.Pass}"`, function (err, result) {
+        db.query('select*from usuario where User = ? and Pass = ?', [data.User, data.Pass], function (err, result) {
             console.log(result);
             if (err) console.log(err);//Se imprime algún error que haya ocurrido
             if (result.length > 0) {//Si sí hizo una búsqueda
@@ -34,14 +34,15 @@ io.on('connection', (socket) => {
     });
 
     // Consultas de productos
-    socket.on('Consul_Prod', async (data) => {
-        console.log('Productos: ', data);
+    socket.on('Consul_Prod', async () => {  
         // Autenticar al usuario utilizando la lógica definida anteriormente
-        db.query(`select*from almacen`, function (err, result) {
-            console.log(result);
+        db.query('select*from almacen', function (err, result) {
             if (err) console.log("Error de búsqueda: " + err);//Se imprime algún error que haya ocurrido
             if (result.length > 0) {//Si sí hizo una búsqueda
-                socket.emit('Productos', { Cod_Barras: result[0].CodBarras, FIngreso: result[0].FecAct });//Mandar usuario y token al cliente
+                console.log(result[1].Cod_Barras);
+                for(var i=0; i <result.length;i++){
+                    socket.emit('Desp_Productos', { Cod_Barras: result[i].Cod_Barras, FIngreso: result[i].FIngreso, Categoria: result[i].Categoria, NArt: result[i].Articulo, NMarca: result[i].Marca, Desc: result[i].Descripcion, Prov: result[i].Proveedor, Unidad: result[i].Unidad, Cant: result[i].Cantidad, NFact: result[i].NFact, Existencia: result[i].Existencia });//Mandar usuario y token al cliente
+                }
             } else {
                 socket.emit('Productos_Inexistentes', { mensaje: 'No hay datos para mostrar' });//Mandar mensaje de error a cliente
             }
@@ -53,18 +54,18 @@ io.on('connection', (socket) => {
     socket.on('Alta_Prod', async (data) => {
         console.log('Productos: ', data);
         //Autentificar que no exista un producto igual
-        db.query(`select*from almacen where Cod_Barras = "${data.CodBarras}"`, function (err, result) {
+        db.query('select*from almacen where Cod_Barras = ?',[data.CodBarras], function (err, result) {
             console.log("El resultado de la consulta es: ", result);
             if (err) console.log("Error de búsqueda: " + err);//Se imprime algún error que haya ocurrido
             console.log(result);
             if (result.length > 0) {//Si sí hizo una búsqueda
                 socket.emit('Producto_Existente', { mensaje: "Este producto ya se encuentra en existencia." });//Mandar usuario y token al cliente
             } else {
-                db.query(`insert into Facturas_Almacen values("${data.NumFactura}", "${data.FechaFac}")`, function (err1, result) {//Insertar factura
+                db.query('insert into Facturas_Almacen values(?,?)',[data.NumFactura,data.FechaFac], function (err1, result) {//Insertar factura
                     if (err1) console.log("Error en inserción de facturas: ", err1);
                     if (result) {
                         //Se agrega productos a la BD
-                        db.query(`insert into almacen values ("${data.CodBarras}","${data.FecAct}","${data.Cate}","${data.Producto}","${data.Marca}","${data.Descripcion}","${data.Proveedor}","${data.NumFactura}","${data.Unidad}","${data.Cantidad}","${data.Cantidad}")`, function (err2, result) {
+                        db.query('insert into almacen values (?,?,?,?,?,?,?,?,?,?,?)', [data.CodBarras,data.FecAct,data.Cate,data.Producto,data.Marca,data.Descripcion,data.Proveedor,data.NumFactura,data.Unidad,data.Cantidad,data.Cantidad], function (err2, result) {
                             console.log("A");
                             if (err2) console.log("Error de inserción de productos: ", err2);
                             if (result) {
