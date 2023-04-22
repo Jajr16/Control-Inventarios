e6486e0c549bc426545e6b2c3d2d94d7a2abdc18
Jajr16 <jajr0316@gmail.com>
2023-04-21 21:09:43 -0600

    Cambios funcional

--- public/javascripts/SocketsC.js
+++ public/javascripts/SocketsC.js
@@ -33,7 +33,7 @@ if (pathname == "/users/altasPro") {
 } else if (pathname == "/users/consulPro") {
 
     socket.emit("Consul_Prod");
-
+    var FechaFactura = "";
     // Consulta de productos
     socket.on('Desp_Productos', async (data) => {
         console.log('Datos recibidos:', data.Cod_Barras);
@@ -48,9 +48,9 @@ if (pathname == "/users/altasPro") {
             <td id="DescripcionP">${data.Desc}</td>
             <td id="Proveedor">${data.Prov}</td>
             <td id="UnidadP">${data.Unidad}</td>
-            <td id="CantidadP">${data.Cant}</td>
             <td id="NumFact">${data.NFact}</td>
             <td id="Existencia">${data.Existencia}</td>
+            <td id="Ffact" style="display:none;">${data.Ffact}</td>
             <td id="Eliminar" class="BotonER"> Eliminar </td>
             <td id="Modificar" class="BotonMod"> Modificar </td>
         </tr>
@@ -72,7 +72,7 @@ if (pathname == "/users/altasPro") {
             }
         })
 
-        $("#DatosProd tbody tr").each(function () { 
+        $("#DatosProd tbody tr").each(function () {
             if ($(this).children(".existe").length > 0) {
                 $(this).show();
             } else {
@@ -116,34 +116,84 @@ if (pathname == "/users/altasPro") {
 
     });
 
-    // Cambios de productos
-    socket.on('ButtonUpdate', () => {
+    socket.on('ButtonUp', () => {
+        let BotonMod = document.getElementsByClassName("BotonMod");
 
-        const BotonBajas = document.getElementsByClassName("BotonAE");
+        for (let i = 0; i < BotonMod.length; i++) {
+            BotonMod[i].addEventListener("click", obtenerValoresMod);
+        }
+
+        //Llenar datos en automático
+        var valores0 = "";
+        var valores1 = "";
+        var valores2 = "";
+        var valores3 = "";
+        var valores4 = "";
+        var valores5 = "";
+        var valores6 = "";
+        var valores7 = "";
+        var valores9 = "";
+        var valores10 = "";
+
+        function obtenerValoresMod(e) {
+
+
+            var elementosTD = e.srcElement.parentElement.getElementsByTagName("td");
+            // recorremos cada uno de los elementos del array de elementos <td>
+            for (let i = 0; i < elementosTD.length; i++) {
+                // obtenemos cada uno de los valores y los ponemos en la variable "valores"
+                valores0 = elementosTD[0].innerHTML;
+                valores1 = elementosTD[1].innerHTML;
+                valores2 = elementosTD[2].innerHTML;
+                valores3 = elementosTD[3].innerHTML;
+                valores4 = elementosTD[4].innerHTML;
+                valores5 = elementosTD[5].innerHTML;
+                valores6 = elementosTD[6].innerHTML;
+                valores7 = elementosTD[7].innerHTML;
+                valores8 = elementosTD[8].innerHTML;
+                valores9 = elementosTD[9].innerHTML;
+                valores10 = elementosTD[10].innerHTML;
+            }
+
+            document.getElementById("FecActuM").value = valores0;
+            document.getElementById("Cod_BarrasM").value = valores1;
+            document.getElementById("CategoriaM").value = valores2;
+            document.getElementById("NomPM").value = valores3;
+            document.getElementById("MarcActiM").value = valores4;
+            document.getElementById("DescripcionPM").value = valores5;
+            document.getElementById("ProveedorM").value = valores6;
+            document.getElementById("UnidadPM").value = valores7;
+            document.getElementById("NumFactM").value = valores8;
+            document.getElementById("FecFactM").value = valores10;
+        }
+
 
         // Cambios de productos
-        BotonBajas.addEventListener("click", Enviar);
+        const FormMod = document.querySelector("#ModProduct");
+
+        // Cambios de productos
+        FormMod.addEventListener("submit", Enviar);
 
         // Comprueba que no haya otro igual
         function Enviar(e) {
+            
             e.preventDefault();
-            if ($("#Cod_Barras").val() != "" && $("#FecActu").val() != "" && $("#Categoria").val() != "" && $("#NomP").val() != "" && $("#MarcActi").val() != "" && $("#DescripcionP").val() != "" && $("#Proveedor").val() != "" && $("#NumFact").val() != "" && $("#CantidadP").val() != "" && $("#UnidadP").val() != "" && $("#FecFact").val()) {
-                socket.emit('Cambios_Prod', { CodBarras: $("#Cod_Barras").val(), FecAct: $("#FecActu").val(), Cate: $("#Categoria").val(), Producto: $("#NomP").val(), Marca: $("#MarcActi").val(), Descripcion: $("#DescripcionP").val(), Proveedor: $("#Proveedor").val(), NumFactura: $("#NumFact").val(), FechaFac: $("#FecFact").val(), Cantidad: $("#CantidadP").val(), Unidad: $("#UnidadP").val() });
-    
-                socket.on('Fact_Exists', function (Respuesta) {
-                    alert(Respuesta.mensaje);
-                });
-    
-                socket.on('Producto_Existente', function (Respuesta) {
-                    alert(Respuesta.mensaje);
-                    location.reload();
-                });
-    
+
+            if ($("#Cod_BarrasM").val() != "" && $("#FecActuM").val() != "" && $("#CategoriaM").val() != "" && $("#NomPM").val() != "" && $("#MarcActiM").val() != "" && $("#DescripcionPM").val() != "" && $("#ProveedorM").val() != "" && $("#NumFactM").val() != "" && $("#CantidadPM").val() != "" && $("#UnidadPM").val() != "" && $("#FecFactM").val()) {
+                socket.emit('Cambios_Prod', { CodBarras: $("#Cod_BarrasM").val(), FecAct: $("#FecActuM").val(), Cate: $("#CategoriaM").val(), Producto: $("#NomPM").val(), Marca: $("#MarcActiM").val(), Descripcion: $("#DescripcionPM").val(), Proveedor: $("#ProveedorM").val(), NumFactura: $("#NumFactM").val(), FechaFac: $("#FecFactM").val(), Existencia: (parseInt($("#CantidadPM").val()) +  parseInt(valores9)), Unidad: $("#UnidadPM").val() }, { FIO: valores0, CBO: valores1, CO: valores2, NAO: valores3, MAO: valores4, DO: valores5, PO: valores6, UO: valores7, NFO: valores8, FFactO: valores10 });
+
                 socket.on('Producto_Inexistente', function (Respuesta) {
                     alert(Respuesta.mensaje);
                     location.reload();
                 });
+
+                socket.on('Fallo_Mod', function (Respuesta) {
+                    alert(Respuesta.mensaje);
+                });
             }
         }
+
     });
+
+
 } 
--- sockServ.js
+++ sockServ.js
@@ -36,14 +36,18 @@ io.on('connection', (socket) => {
     // Consultas de productos
     socket.on('Consul_Prod', async () => {
         // Autenticar que haga las consultas
-        db.query('select*from almacen', function (err, result) {
+        db.query('select almacen.Cod_Barras, almacen.FIngreso, almacen.Categoria, almacen.Articulo, almacen.Marca, almacen.Descripcion, almacen.Unidad, almacen.Existencia, almacen.Proveedor, facturas_almacen.Num_Fact, facturas_almacen.Ffact from almacen natural join facturas_almacen', function (err, result) {
             if (err) console.log("Error de búsqueda: " + err);//Se imprime algún error que haya ocurrido
             if (result.length > 0) {//Si sí hizo una búsqueda
 
                 for (var i = 0; i < result.length; i++) {
-                    socket.emit('Desp_Productos', { Cod_Barras: result[i].Cod_Barras, FIngreso: result[i].FIngreso, Categoria: result[i].Categoria, NArt: result[i].Articulo, NMarca: result[i].Marca, Desc: result[i].Descripcion, Prov: result[i].Proveedor, Unidad: result[i].Unidad, Cant: result[i].Cantidad, NFact: result[i].NFact, Existencia: result[i].Existencia });//Mandar usuario y token al cliente
+                    var FechasIngresos = new Date(result[i].FIngreso);
+                    var Fecha_Factura = new Date(result[i].Ffact);
+
+                    socket.emit('Desp_Productos', { Cod_Barras: result[i].Cod_Barras, FIngreso: FechasIngresos.toISOString().slice(0, 10), Categoria: result[i].Categoria, NArt: result[i].Articulo, NMarca: result[i].Marca, Desc: result[i].Descripcion, Prov: result[i].Proveedor, Unidad: result[i].Unidad, NFact: result[i].Num_Fact, Ffact: Fecha_Factura.toISOString().slice(0, 10), Existencia: result[i].Existencia });//Mandar usuario y token al cliente
                 }
                 socket.emit('ButtonDelete');
+                socket.emit('ButtonUp');
             } else {
                 socket.emit('Productos_Inexistentes', { mensaje: 'No hay datos para mostrar' });//Mandar mensaje de error a cliente
             }
@@ -67,12 +71,12 @@ io.on('connection', (socket) => {
                     if (result.length > 0) {
                         socket.emit('Fact_Exists', { mensaje: "Ese número de factura ya fue agregado con anterioridad." });
                     } else {
-                        db.query('insert into Facturas_Almacen values(?,?)', [data.NumFactura, data.FechaFac], function (err1, result) {//Insertar factura
-                            if (err1) console.log("Error en inserción de facturas: ", err1);
+                        //Se agrega productos a la BD
+                        db.query('insert into almacen values (?,?,?,?,?,?,?,?,?)', [data.CodBarras, data.FecAct, data.Cate, data.Producto, data.Marca, data.Descripcion, data.Proveedor, data.Unidad, data.Cantidad], function (err2, result) {
+                            if (err2) console.log("Error de inserción de productos: ", err2);
                             if (result) {
-                                //Se agrega productos a la BD
-                                db.query('insert into almacen values (?,?,?,?,?,?,?,?,?,?,?)', [data.CodBarras, data.FecAct, data.Cate, data.Producto, data.Marca, data.Descripcion, data.Proveedor, data.NumFactura, data.Unidad, data.Cantidad, data.Cantidad], function (err2, result) {
-                                    if (err2) console.log("Error de inserción de productos: ", err2);
+                                db.query('insert into Facturas_Almacen values(?,?,?,?)', [data.NumFactura, data.FechaFac, data.CodBarras, data.Cantidad], function (err1, result) {//Insertar factura
+                                    if (err1) console.log("Error en inserción de facturas: ", err1);
                                     if (result) {
                                         console.log("Resultado de inserción de productos: ", result);
                                         socket.emit('Producto_Inexistente', { mensaje: 'Producto dado de alta.' });//Mandar mensaje de error a cliente
@@ -81,7 +85,6 @@ io.on('connection', (socket) => {
                             }
                         });
                     }
-
                 });
             }
         });
@@ -112,38 +115,28 @@ io.on('connection', (socket) => {
     });
 
     // Cambios en productos
-    socket.on('Cambios_Prod', async (data) => {
-        //Autentificar que no exista un producto igual
-        db.query('select*from almacen where Cod_Barras = ?', [data.CodBarras], function (err, result) {
+    socket.on('Cambios_Prod', async (data, dataOld) => {
 
-            if (err) console.log("Error de búsqueda: " + err);//Se imprime algún error que haya ocurrido
-
-            if (result.length > 0) {//Si sí hizo una búsqueda
-                socket.emit('Producto_Existente', { mensaje: "Este producto ya se encuentra en existencia." });//Mandar usuario y token al cliente
-            } else {
-                db.query('select*from Facturas_Almacen where Num_Fact = ?', [data.NumFactura], function (err, result) {
-                    if (err) console.log("Error en búsqueda: ", err);
-                    if (result.length > 0) { // Si sí hizo una búsqueda
-                        socket.emit('Fact_Exists', { mensaje: "Ese número de factura ya fue agregado con anterioridad." });
-                    } else { // Si no existe otro igual
-                        db.query('update Facturas_Almacen set values(?,?)', [data.NumFactura, data.FechaFac], function (err1, result) {//Insertar factura
-                            if (err1) console.log("Error en inserción de facturas: ", err1);
-                            if (result) {
-                                //Se agrega productos a la BD
-                                db.query('update almacen set values (?,?,?,?,?,?,?,?,?,?,?)', [data.CodBarras, data.FecAct, data.Cate, data.Producto, data.Marca, data.Descripcion, data.Proveedor, data.NumFactura, data.Unidad, data.Cantidad, data.Cantidad], function (err2, result) {
-                                    if (err2) console.log("Error de inserción de productos: ", err2);
-                                    if (result) {
-                                        console.log("Resultado de inserción de productos: ", result);
-                                        socket.emit('Producto_Inexistente', { mensaje: 'Producto dado de alta.' });//Mandar mensaje de error a cliente
-                                    }
-                                });
-                            }
-                        });
+        //Se agrega productos a la BD
+        db.query('update almacen set Cod_Barras = ?, FIngreso = ?, Categoria = ?, Articulo = ?, Marca = ?, Descripcion = ?, Proveedor = ?, Unidad = ?, Existencia = ? where Cod_Barras = ?', [data.CodBarras, data.FecAct, data.Cate, data.Producto, data.Marca, data.Descripcion, data.Proveedor, data.Unidad, data.Existencia, dataOld.CBO], function (err2, result) {
+            if (err2) console.log("Error de inserción de productos: ", err2);
+            if (result) {
+                db.query('update Facturas_Almacen set Num_Fact = ?, Ffact = ?, Cod_Barras = ? where Num_Fact = ?', [data.NumFactura, data.FechaFac, data.CodBarras, dataOld.NFO], function (err1, result) {//Insertar factura
+                    if (err1) console.log("Error en inserción de facturas: ", err1);
+                    if (result) {
+                        console.log("Resultado de inserción de productos: ", result);
+                        socket.emit('Producto_Inexistente', { mensaje: 'Artículo modificado con éxito.' });//Mandar mensaje a cliente
+                    }else {
+                        socket.emit('Fallo_Mod', {mensaje: "No se pudo modificar el artículo."})
                     }
-
                 });
             }
         });
+
+
+
+
+
         data.length = 0;
     });
 
