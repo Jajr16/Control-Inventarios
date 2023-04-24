9ea1e20dcaeacaf00eb98228ef0a3ea7e0f3fad0
Jajr16 <jajr0316@gmail.com>
2023-04-23 20:13:08 -0600

    Ip

--- public/javascripts/SocketsC.js
+++ public/javascripts/SocketsC.js
@@ -1,5 +1,5 @@
 console.log(localStorage.getItem("token"));
-var socket = io.connect("http://192.168.1.66:3000");
+var socket = io.connect("http://localhost:3000");
 
 var pathname = window.location.pathname;
 
@@ -267,114 +267,9 @@ if (pathname == "/users/altasPro") {
                 });
             }
         }
-    });
-} else if (pathname == "/users/ABPE") {
-
-    socket.emit("Consul_Prod");
-    var FechaFactura = "";
-    // Consulta de productos
-    socket.on('Desp_Productos', async (data) => {
-        console.log('Datos recibidos:', data.Cod_Barras);
-
-        document.querySelector("#DatosProd tbody").innerHTML += `
-        <tr>
-            <td id="F_Ingreso">${data.FIngreso}</td>
-            <td id="Cod_Barras">${data.Cod_Barras}</td>
-            <td id="Categoria">${data.Categoria}</td>
-            <td id="NomP">${data.NArt}</td>
-            <td id="MarcActi">${data.NMarca}</td>
-            <td id="DescripcionP">${data.Desc}</td>
-            <td id="Proveedor">${data.Prov}</td>
-            <td id="UnidadP">${data.Unidad}</td>
-            <td id="NumFact">${data.NFact}</td>
-            <td id="Existencia">${data.Existencia}</td>
-            <td id="Ffact" style="display:none;">${data.Ffact}</td>
-            <td id="Agregar" class="BotonMod"> Añadir al almacén </td>
-            <td id="Eliminar" class="BotonER"> Eliminar del almacén </td>
-        </tr>
-        `;
 
 
     });
 
-    // Barra de busqueda
-    function buscar() {
 
-        var filtro = $("#buscar").val().toUpperCase();
-
-        $("#DatosProd td").each(function () {
-            var textoEnTd = $(this).text().toUpperCase();
-            if (textoEnTd.indexOf(filtro) >= 0) {
-                $(this).addClass("existe");
-            } else {
-                $(this).removeClass("existe");
-            }
-        })
-
-        $("#DatosProd tbody tr").each(function () {
-            if ($(this).children(".existe").length > 0) {
-                $(this).show();
-            } else {
-                $(this).hide();
-            }
-        })
-    }
-    socket.on('ButtonUp', () => {
-        let BotonMod = document.getElementsByClassName("BotonMod");
-
-        for (let i = 0; i < BotonMod.length; i++) {
-            BotonMod[i].addEventListener("click", obtenerValoresMod);
-        }
-
-        //Llenar datos en automático
-        var valores0 = "";
-        var valores1 = "";
-        var valores2 = "";
-        var valores3 = "";
-
-        function obtenerValoresMod(e) {
-
-
-            var elementosTD = e.srcElement.parentElement.getElementsByTagName("td");
-            // recorremos cada uno de los elementos del array de elementos <td>
-            for (let i = 0; i < elementosTD.length; i++) {
-                // obtenemos cada uno de los valores y los ponemos en la variable "valores"
-                valores0 = elementosTD[0].innerHTML;
-                valores1 = elementosTD[9].innerHTML;
-                valores2 = elementosTD[8].innerHTML;
-                valores3 = elementosTD[10].innerHTML;
-            }
-
-            document.getElementById("FecActuM").value = valores0;
-            document.getElementById("CantidadPM").value = valores1;
-            document.getElementById("NumFactM").value = valores2;
-            document.getElementById("FecFactM").value = valores3;
-        }
-
-
-        // Cambios de productos
-        const FormMod = document.querySelector("#ModProduct");
-
-        // Cambios de productos
-        FormMod.addEventListener("submit", Enviar);
-
-        // Comprueba que no haya otro igual
-        function Enviar(e) {
-            
-            e.preventDefault();
-
-            if ($("#FecActuM").val() != "" && $("#NumFactM").val() != "" && $("#CantidadPM").val() != "" && $("#CantidadPM").val() != 0 && $("#FecFactM").val()) {
-                socket.emit('Cambios_Prod', {FecAct: $("#FecActuM").val(), NumFactura: $("#NumFactM").val(), FechaFac: $("#FecFactM").val(), Existencia: (parseInt($("#CantidadPM").val()) +  parseInt(valores1))}, { FIO: valores0, NFO: valores2, FFactO: valores3 });
-
-                socket.on('Producto_Inexistente', function (Respuesta) {
-                    alert(Respuesta.mensaje);
-                    location.reload();
-                });
-
-                socket.on('Fallo_Mod', function (Respuesta) {
-                    alert(Respuesta.mensaje);
-                });
-            }
-        }
-    });
 } 
