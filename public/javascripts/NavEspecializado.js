f61cfd3aa847a76314e72e44d137dcc776a2d931
W1NTER <83407276+ShabbyRug775@users.noreply.github.com>
2023-09-15 03:02:25 -0600

    Mejora de Nav2

--- public/javascripts/NavEspecializado.js
+++ public/javascripts/NavEspecializado.js
@@ -10,13 +10,13 @@ document.getElementById("Linksnav").innerHTML += `
 `;
 
 // Usuarios con permisos de Almacén
-if (Permisos['ALMACEN']) {
+if (Permisos['ALMACÉN']) {
     // Permisos de altas
     if (Permisos['ALMACÉN'].includes('1')) {
         document.getElementById("Linksnav").innerHTML += `
         <li>
             <p>Registrar productos</p>
-            <ul class="menu-vertical_nav">
+            <ul class="menu-vertical_nav" id = "Alma1">
                 <li>
                     <div><a href="/users/ABPE">Agregar productos existentes</a></div> 
                 </li>
@@ -29,17 +29,12 @@ if (Permisos['ALMACEN']) {
     }
     // Permisos de Bajas, Cambios y Consultas
     if (Permisos['ALMACÉN'].includes('2') || Permisos['ALMACÉN'].includes('3') || Permisos['ALMACÉN'].includes('4')) {
-        document.getElementById("Linksnav").innerHTML += `
+        document.getElementById("Alma1").innerHTML += `
         <li>
-            <p>Registrar productos</p>
-            <ul class="menu-vertical_nav">
-                <li>
-                    <div><a href="/users/consulPro">Ver productos</a></div> 
-                </li>
-                <li>
-                    <div><a href="/users/FacSacProd">Registro de productos sacados</a></div> 
-                </li>
-            </ul>
+            <div><a href="/users/consulPro">Ver productos</a></div> 
+        </li>
+        <li>
+            <div><a href="/users/FacSacProd">Registro de productos sacados</a></div> 
         </li>
     `;
     }
@@ -51,7 +46,7 @@ if (Permisos['MOBILIARIO']) {
         document.getElementById("Linksnav").innerHTML += `
         <li>
             <p>Registrar mobiliario</p>
-            <ul class="menu-vertical_nav">
+            <ul class="menu-vertical_nav" id = "Mob1">
                 <li>
                     <div><a href="/users/altasMob">Registrar Mobiliario</a></div> 
                 </li>
@@ -61,14 +56,9 @@ if (Permisos['MOBILIARIO']) {
     }
     // Permisos de Bajas, Cambios y Consultas
     if (Permisos['MOBILIARIO'].includes('2') || Permisos['MOBILIARIO'].includes('3') || Permisos['MOBILIARIO'].includes('4')) {
-        document.getElementById("Linksnav").innerHTML += `
+        document.getElementById("Mob1").innerHTML += `
         <li>
-            <p>Registrar mobiliario</p>
-            <ul class="menu-vertical_nav">
-                <li>
-                    <div><a href="/users/consulMob">Ver Mobiliario</a></div> 
-                </li>
-            </ul>
+            <div><a href="/users/consulMob">Ver Mobiliario</a></div> 
         </li>
     `;
     }
@@ -80,7 +70,7 @@ if (Permisos['EQUIPOS']) {
         document.getElementById("Linksnav").innerHTML += `
         <li>
             <p>Registrar equipos</p>
-            <ul class="menu-vertical_nav">
+            <ul class="menu-vertical_nav" id = "Eqp1">
                 <li>
                     <div><a href="/users/altasEqp">Registrar Equipos</a></div> 
                 </li>
@@ -90,14 +80,9 @@ if (Permisos['EQUIPOS']) {
     }
     // Permisos de Bajas, Cambios y Consultas
     if (Permisos['EQUIPOS'].includes('2') || Permisos['EQUIPOS'].includes('3') || Permisos['EQUIPOS'].includes('4')) {
-        document.getElementById("Linksnav").innerHTML += `
+        document.getElementById("Eqp1").innerHTML += `
         <li>
-            <p>Registrar equipos</p>
-            <ul class="menu-vertical_nav">
-                <li>
-                    <div><a href="/users/consulEqp">Ver Equipos</a></div> 
-                </li>
-            </ul>
+            <div><a href="/users/consulEqp">Ver Equipos</a></div> 
         </li>
     `;
     }
@@ -124,7 +109,7 @@ if (Permisos['USUARIOS']) {
         document.getElementById("Linksnav").innerHTML += `
         <li>
             <p>Más opciones</p>
-            <ul class="menu-vertical_nav">
+            <ul class="menu-vertical_nav" id = "UseEmp1">
                 <li>
                     <div><a href="/users/RegistrarUsuario">Registrar Usuario</a></div> 
                 </li>
@@ -134,14 +119,9 @@ if (Permisos['USUARIOS']) {
     }
     // Permisos de Bajas, Cambios y Consultas
     if ((Permisos['USUARIOS'].includes('2') || Permisos['USUARIOS'].includes('3') || Permisos['USUARIOS'].includes('4'))) {
-        document.getElementById("Linksnav").innerHTML += `
+        document.getElementById("UseEmp1").innerHTML += `
         <li>
-            <p>Más opciones</p>
-            <ul class="menu-vertical_nav">
-                <li>
-                    <div><a href="/users/consulUsuarios">Modificar Usuario</a></div> 
-                </li>
-            </ul>
+            <div><a href="/users/consulUsuarios">Modificar Usuario</a></div> 
         </li>
         `;
     }
@@ -150,27 +130,17 @@ if (Permisos['USUARIOS']) {
 if (Permisos['EMPLEADOS']) {
     // Permisos de altas
     if (Permisos['EMPLEADOS'].includes('1')) {
-        document.getElementById("Linksnav").innerHTML += `
+        document.getElementById("UseEmp1").innerHTML += `
         <li>
-            <p>Más opciones</p>
-            <ul class="menu-vertical_nav">
-                <li>
-                    <div><a href="/users/RegistroEmpleado">Registrar Empleado</a></div> 
-                </li>
-            </ul>
+            <div><a href="/users/RegistroEmpleado">Registrar Empleado</a></div> 
         </li>
     `;
     }
     // Permisos de Bajas, Cambios y Consultas
     if (Permisos['EMPLEADOS'].includes('2') || Permisos['EMPLEADOS'].includes('3') || Permisos['EMPLEADOS'].includes('4')) {
-        document.getElementById("Linksnav").innerHTML += `
+        document.getElementById("UseEmp1").innerHTML += `
         <li>
-            <p>Más opciones</p>
-            <ul class="menu-vertical_nav">
-                <li>
-                    <div><a href="/users/ModEmp">Modificar Empleado</a></div> 
-                </li>
-            </ul>
+            <div><a href="/users/ModEmp">Modificar Empleado</a></div> 
         </li>
     `;
     }
