f00718db514361a073cf0335c44ffd51274c33c3
W1NTER <83407276+ShabbyRug775@users.noreply.github.com>
2023-09-15 01:08:12 -0600

    Cambios en el nav
    Ahora le aparece solo un determinado contenido dependiendo los permisos del usuario.

--- public/javascripts/NavEspecializado.js
+++ public/javascripts/NavEspecializado.js
@@ -1,79 +1,100 @@
-var tok = localStorage.getItem("token");
+// Variable de permisos
+var Permisos = JSON.parse(localStorage.getItem('permisosModulos'));
 
+// Esto aplica para todos los usuarios
 document.getElementById("Linksnav").innerHTML += `
     <li>
         <a href="/users/index">Inicio</a>
     </li>
-    <li>
-        <p>Registrar productos</p>
-        <ul class="menu-vertical_nav">
-        <li>
-            <div><a href="/users/consulPro">Ver productos</a></div> 
-        </li>
-        <li>
-            <div><a href="/users/ABPE">Agregar productos existentes</a></div> 
-        </li>
-        <li>
-            <div><a href="/users/altasPro">Registrar Productos</a></div> 
-        </li>
-        <li>
-            <div><a href="/users/FacSacProd">Registro de productos sacados</a></div> 
-        </li>
-        </ul>
-    </li>
-    <li>
-        <p>Registrar mobiliario</p>
-        <ul class="menu-vertical_nav">
-        <li>
-            <div><a href="/users/consulMob">Ver Mobiliario</a></div> 
-        </li>
-        <li>
-            <div><a href="/users/altasMob">Registrar Mobiliario</a></div> 
-        </li>
-        </ul>
-    </li>
-    <li>
-        <p>Registrar equipos</p>
-        <ul class="menu-vertical_nav">
-        <li>
-            <div><a href="/users/consulEqp">Ver Equipos</a></div> 
-        </li>
-        <li>
-            <div><a href="/users/altasEqp">Registrar Equipos</a></div> 
-        </li>
-        </ul>
-    </li>
-    <li>
-        <p>Crear responsivas</p>
-        <ul class="menu-vertical_nav">
-        <li>
-            <div><a href="/users/crear_resp">Responsivas</a></div> 
-        </li>
-        </ul>
-    </li>
-    <li>
-        <p>Más opciones</p>
-        <ul class="menu-vertical_nav">
-        <li>
-            <div><a href="/users/RegistrarUsuario">Registrar Usuario</a></div> 
-        </li>
-        <li>
-            <div><a href="/users/consulUsuarios">Modificar Usuario</a></div> 
-        </li>
-        <li>
-            <div><a href="/users/RegistroEmpleado">Registrar Empleado</a></div> 
-        </li>
-        <li>
-            <div><a href="/users/ModEmp">Modificar Empleado</a></div> 
-        </li>
-        </ul>
-    </li>
-        `;
+`;
 
-if (tok == "4dnM3k0nl9s" || tok == "4dnM3k0nl9z") {
-
-    document.getElementById("MenuCel").innerHTML += `
-            <a href="/users/RegistrarUsuario">Registrar Usuario</a>
-            <a href="/users/RegistroEmpleado">Registrar Empleado</a>
-        `;
+// Usuarios con permisos de Almacén
+if(Permisos['ALMACÉN'].includes('1') || Permisos['ALMACÉN'].includes('2') || Permisos['ALMACÉN'].includes('3') || Permisos['ALMACÉN'].includes('4')){
+    document.getElementById("Linksnav").innerHTML += `
+        <li>
+            <p>Registrar productos</p>
+            <ul class="menu-vertical_nav">
+            <li>
+                <div><a href="/users/consulPro">Ver productos</a></div> 
+            </li>
+            <li>
+                <div><a href="/users/ABPE">Agregar productos existentes</a></div> 
+            </li>
+            <li>
+                <div><a href="/users/altasPro">Registrar Productos</a></div> 
+            </li>
+            <li>
+                <div><a href="/users/FacSacProd">Registro de productos sacados</a></div> 
+            </li>
+            </ul>
+        </li>
+    `;
+}
+// Usuarios con permisos de Mobiliario
+if(Permisos['MOBILIARIO'].includes('1') || Permisos['MOBILIARIO'].includes('2') || Permisos['MOBILIARIO'].includes('3') || Permisos['MOBILIARIO'].includes('4')){
+    document.getElementById("Linksnav").innerHTML += `
+        <li>
+            <p>Registrar mobiliario</p>
+            <ul class="menu-vertical_nav">
+            <li>
+                <div><a href="/users/consulMob">Ver Mobiliario</a></div> 
+            </li>
+            <li>
+                <div><a href="/users/altasMob">Registrar Mobiliario</a></div> 
+            </li>
+            </ul>
+        </li>
+    `;
+}
+// Usuarios con permisos de Equipos
+if(Permisos['EQUIPOS'].includes('1') || Permisos['EQUIPOS'].includes('2') || Permisos['EQUIPOS'].includes('3') || Permisos['EQUIPOS'].includes('4')){
+    document.getElementById("Linksnav").innerHTML += `
+        <li>
+            <p>Registrar equipos</p>
+            <ul class="menu-vertical_nav">
+            <li>
+                <div><a href="/users/consulEqp">Ver Equipos</a></div> 
+            </li>
+            <li>
+                <div><a href="/users/altasEqp">Registrar Equipos</a></div> 
+            </li>
+            </ul>
+        </li>
+    `;
+}
+// Usuarios con permisos de Responsivas
+if(Permisos['RESPONSIVAS'].includes('1') || Permisos['RESPONSIVAS'].includes('2') || Permisos['RESPONSIVAS'].includes('3') || Permisos['RESPONSIVAS'].includes('4')){
+    document.getElementById("Linksnav").innerHTML += `
+        <li>
+            <p>Crear responsivas</p>
+            <ul class="menu-vertical_nav">
+            <li>
+                <div><a href="/users/crear_resp">Responsivas</a></div> 
+            </li>
+            </ul>
+        </li>
+    `;
+}
+// Usuarios con permisos de modificar Usuarios y empleados
+if((Permisos['USUARIOS'].includes('1') || Permisos['USUARIOS'].includes('2') || Permisos['USUARIOS'].includes('3') || Permisos['USUARIOS'].includes('4')) && 
+(Permisos['EMPLEADOS'].includes('1') || Permisos['EMPLEADOS'].includes('2') || Permisos['EMPLEADOS'].includes('3') || Permisos['EMPLEADOS'].includes('4'))){
+    document.getElementById("Linksnav").innerHTML += `
+        <li>
+            <p>Más opciones</p>
+            <ul class="menu-vertical_nav">
+            <li>
+                <div><a href="/users/RegistrarUsuario">Registrar Usuario</a></div> 
+            </li>
+            <li>
+                <div><a href="/users/consulUsuarios">Modificar Usuario</a></div> 
+            </li>
+            <li>
+                <div><a href="/users/RegistroEmpleado">Registrar Empleado</a></div> 
+            </li>
+            <li>
+                <div><a href="/users/ModEmp">Modificar Empleado</a></div> 
+            </li>
+            </ul>
+        </li>
+    `;
 }
