599404241036121ef3da070ef634771e0270bba8
Jajr16 <jajr0316@gmail.com>
2023-09-14 19:41:18 -0600

    Pequeñas correciones

--- public/javascripts/ValidarSesion.js
+++ public/javascripts/ValidarSesion.js
@@ -5,7 +5,7 @@
 // console.log(localStorage.getItem("token"));
 
 // Esta línea muestra en la consola el contenido almacenado en la clave 'permisosModulos' del almacenamiento local.
-console.log(JSON.parse(localStorage.getItem('permisosModulos')));
+//console.log(JSON.parse(localStorage.getItem('permisosModulos')));
 
 // Esta estructura condicional verifica si la clave 'permisosModulos' en el almacenamiento local es nula (null).
 // Si es nula, redirige a la página de inicio (ruta '/').
